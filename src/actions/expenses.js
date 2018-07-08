594ec49c1c5b7fef3e6cd2e5be6753000b74507b
Leslie Alldridge <lesliealldridge@Lisas-MacBook.local>
2018-07-08 15:52:46 +1200

    breaking into components

---
+++ src/actions/expenses.js
@@ -0,0 +1,34 @@
+import uuid from 'uuid';
+
+//Add expense 
+export const addExpense = ({ 
+    description = '', 
+    note = '', 
+    amount = 0, 
+    createAt = 0
+        } = {}
+    ) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createAt
+    }
+});
+
+//Remove expense
+
+export const removeExpense = ({ id }) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+});
+
+//edit expense 
+
+export const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
