161abad6da12a7ebf11e8394544cd7e9fecf9091
Leslie Alldridge <lesliealldridge@Lisas-MacBook.local>
2018-07-09 20:50:53 +1200

    added remove button on each page

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -3,18 +3,19 @@ import moment from 'moment';
 import { SingleDatePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
 
-const now = moment();
-console.log(now.format('Do MMM, YYYY'));
-
 export default class ExpenseForm extends React.Component {
-    state = {
-        description: '',
-        note: '',
-        amount: '',
-        createdAt: moment(),
+    constructor(props) {
+        super(props);
+    
+    this.state = {
+        description: props.expense ? props.expense.description : '',
+        note: props.expense ? props.expense.note : '',
+        amount: props.expense ? (props.expense.amount / 100).toString() : '',
+        createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
         calendarFocused: false,
         error: ''
     };
+}
     onDescriptionChange = (e) => {
         const description = e.target.value;
         this.setState(() => ({description}));
@@ -90,4 +91,5 @@ export default class ExpenseForm extends React.Component {
             </div>
         )
     }
+
 }
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses';
+import { Link } from 'react-router-dom';
+
 
 // export a stateless function component
 //description, amount, createdAt
 
-const ExpenseListItem = ({dispatch, id, description, amount, createAt}) => (
+const ExpenseListItem = ({ id, description, amount, createAt}) => (
     <div> 
+        <Link to={`/edit/${id}`}>
         <h3>{description}</h3>
+        </Link>
         <p>{amount} - {createAt}</p>
-        <button onClick={() => {
-            dispatch(removeExpense({ id }));
-        }}>Remove</button>
     </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
