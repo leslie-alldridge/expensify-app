127a5ffeeb3acae45716a3b18fbc08c0bf27f82d
Leslie Alldridge <lesliealldridge@Lisas-MacBook.local>
2018-07-11 14:34:37 +1200

    added more expense form tests

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -23,3 +23,46 @@ test('should render error for invalid form submission', () => {
   expect(wrapper).toMatchSnapshot();
 });
 
+test('should set description for input change', () => {
+  const value = 'New Description';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(0).simulate('change', {
+    target: {value}
+  });
+  expect(wrapper.state('description')).toBe(value);
+});
+
+test('should set description for textarea change', () => {
+  const value = 'New Description';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('textarea').simulate('change', {
+    target: {value}
+  });
+  expect(wrapper.state('note')).toBe(value);
+});
+
+test('should set amount for valid input', () => {
+  const value = '12.50';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(1).simulate('change', {
+    target: {value}
+  });
+  expect(wrapper.state('amount')).toBe(value);
+});
+
+test('should not set description for invalid input', () => {
+  const value = '12.122';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(1).simulate('change', {
+    target: {value}
+  });
+  expect(wrapper.state('amount')).toBe('');
+});
+
+test('should call onSubmit prop for valid for submission', () => {
+  const onSubmitSpy = jest.fn();
+  const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy}/>);
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => {}
+  });
+});
