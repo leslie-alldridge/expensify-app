e54eccbd881007a0336700944a7ec7ab83b13e50
Leslie Alldridge <lesliealldridge@Lisas-MacBook.local>
2018-07-11 19:43:01 +1200

    added more test cases

--- src/tests/__mocks__/moment.js
+++ src/tests/__mocks__/moment.js
@@ -1,6 +1,5 @@
 const moment = require.requireActual('moment');
-//cannot import moment for mock testing
-//below says if there's a timestamp use it, otherwise set to 0
+
 export default (timestamp = 0) => {
   return moment(timestamp);
 };
