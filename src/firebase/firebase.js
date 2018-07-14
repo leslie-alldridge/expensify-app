0c11d85696b059ecfebaacb4c261fb26129ac7a1
Leslie Alldridge <lesliealldridge@Lisas-MacBook.local>
2018-07-14 17:04:58 +1200

    set up test database and environment variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,32 +1,34 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyBzijTedq7S52-J72DJhQqGmc6YoLiA9gw",
-    authDomain: "expensify-1eaff.firebaseapp.com",
-    databaseURL: "https://expensify-1eaff.firebaseio.com",
-    projectId: "expensify-1eaff",
-    storageBucket: "expensify-1eaff.appspot.com",
-    messagingSenderId: "1042268364216"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref('expenses')
-   .on('child_removed', (snapshot) => {
-     console.log(snapshot.key, snapshot.val());
-   });
+export { firebase, database as default };
 
-   database.ref('expenses')
-   .on('child_changed', (snapshot) => {
-     console.log(snapshot.key, snapshot.val());
-   });
+// database.ref('expenses')
+//    .on('child_removed', (snapshot) => {
+//      console.log(snapshot.key, snapshot.val());
+//    });
 
-   database.ref('expenses')
-   .on('child_added', (snapshot) => {
-     console.log(snapshot.key, snapshot.val());
-   });
+//    database.ref('expenses')
+//    .on('child_changed', (snapshot) => {
+//      console.log(snapshot.key, snapshot.val());
+//    });
+
+//    database.ref('expenses')
+//    .on('child_added', (snapshot) => {
+//      console.log(snapshot.key, snapshot.val());
+//    });
 // database.ref('expenses')
 //   .on('value', (snapshot) => {
 //     const expenses = [];
