5f0865de692e53b715b6eaef72e4edeb40d119fd
Leslie Alldridge <lesliealldridge@Lisas-MacBook.local>
2018-07-14 17:34:20 +1200

    added more tests and loading status

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -11,19 +11,21 @@ export const startAddExpense = (expenseData = {}) => {
   return (dispatch) => {
     const {
       description = '',
-    note = '',
-    amount = 0,
-    createdAt = 0
+      note = '',
+      amount = 0,
+      createdAt = 0
     } = expenseData;
     const expense = { description, note, amount, createdAt };
+
     return database.ref('expenses').push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
         ...expense
-    }));
-  });
+      }));
+    });
+  };
 };
-};
+
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
@@ -36,3 +38,26 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database.ref('expenses').once('value').then((snapshot) => {
+      const expenses = [];
+
+      snapshot.forEach((childSnapshot) => {
+        expenses.push({
+          id: childSnapshot.key,
+          ...childSnapshot.val()
+        });
+      });
+
+      dispatch(setExpenses(expenses));
+    });
+  };
+};
