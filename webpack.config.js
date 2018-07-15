609dcc3f90a86dd4dba266dfe932007705b98324
Leslie Alldridge <lesliealldridge@Lisas-MacBook.local>
2018-07-15 19:45:01 +1200

    added polyfill and loading animations + styles

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,8 @@ module.exports = (env) => {
   const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
-    entry: './src/app.js',
-  output: {
+    entry: ['babel-polyfill', './src/app.js'],
+    output: {
     path: path.join(__dirname, 'public', 'dist'),
     filename: 'bundle.js'
   },
